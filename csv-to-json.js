ed2b4f3d5b6d1eba9206381b4995263e86b571f2
Adam Smith <atsmith227@gmail.com>
2015-07-21 14:23:41 -0600

    Finished Exercise

--- csv-to-json.js
+++ csv-to-json.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 var _ = require('lodash');
 var csvInput = process.argv[2];
+var jsonOutput = process.argv[3];
 
 var columns;
 var rows;
+var jsonResult = [];
 
 // Read the first line of the csv
 var getColumns = function(input) {
@@ -29,18 +31,14 @@ getRows(csvInput);
 
 
 // Create a array of objects with the columns as keys and the rows as values
-var objectify = function() {
-    var result = rows.forEach(function(row, index){
-        _.zipObject(columns, rows[index]);
-    });
-    
-    return result;
-}
+// Push into our jsonResult Array
+rows.forEach(function(row, index){
+    var obj = _.zipObject(columns, rows[index]);
+    jsonResult.push(obj);
+});
 
-console.log(objectify());
+// Reassign the array to a strinified version
+jsonResult = JSON.stringify(jsonResult);
 
-
-//{
-//    column1: 'Some Value',
-//    column2: 'Another Value'
-//}
+// Write the JSON to a new file as specified in the arguments
+fs.writeFileSync(jsonOutput, jsonResult);
